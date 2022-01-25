bb9aeec4f98f0a6bacc16b3d3e38307209641c9f
Igor Shaymukhametov <shaym@yandex.ru>
2022-01-25 16:54:20 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let k = str.length; k >= 0; k--) {
+
+    for(let j = 0; j < bracketsConfig.length; j++) {
+
+      if(str[k] === bracketsConfig[j][0]) {
+        str = str.replace(bracketsConfig[j].join(''),'')
+      }
+
+    }
+
+  }
+
+  return str === ""
 }
+
